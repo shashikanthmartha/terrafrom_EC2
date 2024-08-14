e9ad7354995794d36432abc7f324656c5b17a8eb
shashikanthmartha <108862566+shashikanthmartha@users.noreply.github.com>
2024-08-14 15:41:41 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -31,12 +31,12 @@ resource "aws_default_vpc" "default" {
   }
 }
 #resource "tls_private_key" "pk" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
+#  algorithm = "RSA"
+# rsa_bits  = 4096
+#}
 
-resource "aws_key_pair" "kp" {
-  key_name   = "myKey"       # Create a "myKey" to AWS!!
-  public_key = tls_private_key.pk.public_key_openssh
-}
+#resource "aws_key_pair" "kp" {
+ # key_name   = "myKey"       # Create a "myKey" to AWS!!
+  #public_key = tls_private_key.pk.public_key_openssh
+#}
 #
