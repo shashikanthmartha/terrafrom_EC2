da1279cde5a19c4f11d8e34041bac3ec3584b7fd
shashikanthmartha <108862566+shashikanthmartha@users.noreply.github.com>
2024-08-14 15:49:59 +0530

    Update security_group.tf

--- security_group.tf
+++ security_group.tf
@@ -8,21 +8,21 @@ resource "aws_security_group" "shashi_ec2" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+resource "aws_vpc_security_group_ingress_rule" "ssh" {
   security_group_id = aws_security_group.shashi_ec2.id
   cidr_ipv4         = aws_default_vpc.default.cidr_block
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
 }
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+resource "aws_vpc_security_group_ingress_rule" "htpps" {
   security_group_id = aws_security_group.shashi_ec2.id
   cidr_ipv4         = aws_default_vpc.default.cidr_block
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
 }
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+resource "aws_vpc_security_group_ingress_rule" "http" {
   security_group_id = aws_security_group.shashi_ec2.id
   cidr_ipv4         = aws_default_vpc.default.cidr_block
   from_port         = 80
@@ -37,8 +37,3 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv6         = "::/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
