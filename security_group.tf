32b8a1ea94b23e6f8b3123cc6fb5c8917c2d9cb5
shashikanthmartha <108862566+shashikanthmartha@users.noreply.github.com>
2024-08-14 15:16:09 +0530

    Update security_group.tf

--- security_group.tf
+++ security_group.tf
@@ -1,31 +1,38 @@
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
+resource "aws_security_group" "shashi_ec2" {
+  name        = "shashi_ec2"
   description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = aws_default_vpc.default.id
 
   tags = {
-    Name = "allow_tls"
+    Name = "shashi_ec2"
   }
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = aws_vpc.main.cidr_block
+  security_group_id = aws_security_group.shashi_ec2.id
+  cidr_ipv4         = aws_default_vpc.default.cidr_block
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.shashi_ec2.id
+  cidr_ipv4         = aws_default_vpc.default.cidr_block
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
+}
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.shashi_ec2.id
+  cidr_ipv4         = aws_default_vpc.default.cidr_block
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
-}
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.shashi_ec2.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
