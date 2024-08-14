a0a45e3eacb6169f81c5392b18fbd1cf293b64db
shashikanthmartha <108862566+shashikanthmartha@users.noreply.github.com>
2024-08-14 15:23:46 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -17,6 +17,8 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "shashi2" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
+  security_groups = []
+  
 
   tags = {
     Name = "shashi-1"
